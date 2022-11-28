67a80d193e7056fd5c031a83076bffb0459ac822
Felixnightshade <61640983+Felixnightshade@users.noreply.github.com>
2022-11-28 16:14:39 -0500

    clarify main

--- musicrecplus.py
+++ musicrecplus.py
@@ -12,64 +12,69 @@ Team: Crazy Coders
 
 
 def main():
-    '''Where the main loop is run -Marcus'''
+    """Where the main loop is run -Marcus"""
 
-    userDict = loadDataBase('musicrecplus_ex2_a.txt')
-    
+    userDict = loadDataBase("musicrecplus_ex2_a.txt")
+
     while True:
-        option = input('Enter a letter to choose an option:' + '\n' + '\t' + 'e - enter preferences' + '\n' + '\t' 'r - get recommendations' + '\n' + '\t' \
-           + 'p - show most popular artists'  + '\n' + '\t' + 'h - how popular is the most popular' + \
-           '\n' + '\t' + 'm - which user has the most likes ' + '\n' + '\t' + 'q - save and quit' +'\n')
+        option = input(
+            """
+            Enter a letter to choose an option:\n
+            \te - Enter preferences\n
+            \tr - Get recommendations\n
+            \tp - Show most popular artists\n
+            \th - How popular is the most popular\n
+            \tm - Which user has the most likes\n
+            \tq - Save and quit\n
+            """
+        )
 
-        if option == 'e':
+        if option == "e":
             pass
-        if option =='r':
+        elif option == "r":
             pass
-            
-        if option == 'p':
-           print(mostPopular(userDict))
-        
-        if option == 'h':
+        elif option == "p":
+            print(mostPopular(userDict))
+        elif option == "h":
             print(howPopular(userDict))
-            
-        if option == 'm':
+        elif option == "m":
             pass
-            
-        if option == 'q':
-            return None
-        
-        if option not in ['e', 'r', 'p', 'h', 'm', 'q']:
+        elif option == "q":
+            return
+        else:
             print("That is not an option.")
 
 
 def loadDataBase(filename):
-    '''loads the database from the file named musicrecplus.txt,
+    """loads the database from the file named musicrecplus.txt,
     if it exists. Otherwise it creates the file. Checks if user is
-    in the database, if not it adds to database along with their preferences- Marcus'''
+    in the database, if not it adds to database along with their preferences- Marcus"""
     myfile = open(filename, "r")
     database = {}
-    name = input('Enter your name (put a $ symbol after your name if you wish your preferences to remain private): \n')
+    name = input(
+        "Enter your name (put a $ symbol after your name if you wish your preferences to remain private): \n"
+    )
     for x in myfile:
-        [userName, artists] = x.strip().split(':')
-        #seperates contents of file into userName list and artists list
-        artistList = artists.split(',')
-        #creates an artistList of each artist that are seperated by commas
+        [userName, artists] = x.strip().split(":")
+        # seperates contents of file into userName list and artists list
+        artistList = artists.split(",")
+        # creates an artistList of each artist that are seperated by commas
         artistList.sort()
         database[userName] = artistList
-        #assigns username as key and artistlist as value in the database dictionary
+        # assigns username as key and artistlist as value in the database dictionary
     myfile.close()
-    aList=[]
+    aList = []
     if name not in database:
-        #if the user is not already in the database, it asks for their preferences
+        # if the user is not already in the database, it asks for their preferences
         while True:
-            artist = input('Enter an artist that you like (Enter to finish): \n')
-            if artist != '':
+            artist = input("Enter an artist that you like (Enter to finish): \n")
+            if artist != "":
                 aList.append(artist.title())
             else:
                 break
         aList.sort()
-        database[name]=aList
-        #adds the new user and their preferences to dictionary
+        database[name] = aList
+        # adds the new user and their preferences to dictionary
     return database
 
 
@@ -81,46 +86,49 @@ def recommendations(username: str, database: dict) -> tuple:
     # TODO Implement recommendations
     pass
 
+
 def mostPopularHelper(userDict):
-    '''returns the artist that occurs in users preferences the most. Is a helper for
-    most popular function -Marcus'''
+    """returns the artist that occurs in users preferences the most. Is a helper for
+    most popular function -Marcus"""
     artistPopularity = {}
     for x in userDict:
-        if x[-1] != '$':
-            #excludes users who are private
+        if x[-1] != "$":
+            # excludes users who are private
             for i in userDict[x]:
                 if i not in artistPopularity:
                     artistPopularity[i] = 1
-                    #if artist is not in the dictionary, it adds one instance to the new dictionary
+                    # if artist is not in the dictionary, it adds one instance to the new dictionary
                 else:
-                    artistPopularity[i] +=1
-                    #if artist is already in dictionary, value of 1 is added to the artist count
-    return sorted(artistPopularity.items(),key=lambda x:x[1],reverse=True)
-    #returns a list sorted by value from greatest to least, so most popular artist will be at index 0, least will be at the end
+                    artistPopularity[i] += 1
+                    # if artist is already in dictionary, value of 1 is added to the artist count
+    return sorted(artistPopularity.items(), key=lambda x: x[1], reverse=True)
+    # returns a list sorted by value from greatest to least, so most popular artist will be at index 0, least will be at the end
+
 
 def mostPopular(userDict):
-    '''returns the top 3 artists that appear in users preferences. -Marcus'''
+    """returns the top 3 artists that appear in users preferences. -Marcus"""
     mostPop = mostPopularHelper(userDict)[:3]
-    a=''
-    #takes the top 3 most popular artists
+    a = ""
+    # takes the top 3 most popular artists
     for x in mostPop:
-        if x =='':
-            a = 'Sorry, no artists found'
-        a += (str(x[0])+'\n')
+        if x == "":
+            a = "Sorry, no artists found"
+        a += str(x[0]) + "\n"
     return a
 
+
 def howPopular(userDict):
-    '''returns how popular the most popular artist is. -Marcus'''
+    """returns how popular the most popular artist is. -Marcus"""
     x = mostPopularHelper(userDict)[0]
-    a=''
-    if x=='':
-        a = 'Sorry, no artists found'
+    a = ""
+    if x == "":
+        a = "Sorry, no artists found"
     a = str(x[1])
     return a
 
+
 def enterPreferences():
-    '''guides user to enter their preferences
-    '''
+    """guides user to enter their preferences"""
 
 
 def highestPopularity(database: dict) -> int:
